d011732c3e51261c988ddbb302e71a1317dd7b0c
athakuri123 <45645377+athakuri123@users.noreply.github.com>
2021-11-03 22:25:41 -0400

    Student entry conditionals

--- src/main/java/com/experimental/StudentRegistration/Service/StudentEntryService.java
+++ src/main/java/com/experimental/StudentRegistration/Service/StudentEntryService.java
@@ -1,9 +1,6 @@
 package com.experimental.StudentRegistration.Service;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -18,47 +15,50 @@ public class StudentEntryService
     static final String USER = "root";
     static final String PASS = "m@nepAli2";
 
-    public static void main(String[] args) throws SQLException, IOException
-    {
-        //Get entry data from user
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter First Name:");
-        String fName = scanner.nextLine();  // Read user input
-        System.out.println("Enter Last Name:");
-        String lName = scanner.nextLine();  // Read user input
-        System.out.println("Enter Major:");
-        String major = scanner.nextLine();  // Read user input
-
+    public static void main(String[] args) throws SQLException, IOException {
         Logger logger = Logger.getLogger(StudentEntryService.class.getName());
         FileHandler fh;
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter 1 to continue or 2 to end:");
+        String userIn = scanner.nextLine();
+        if (userIn.equals("1")) {
+                //Get entry data from user
+                System.out.println("Enter First Name:");
+                String fName = scanner.nextLine();  // Read user input
+                System.out.println("Enter Last Name:");
+                String lName = scanner.nextLine();  // Read user input
+                System.out.println("Enter Major:");
+                String major = scanner.nextLine();  // Read user input
 
-        //Database entry to use in data entry service
-        String entry = "INSERT INTO Registration.Students (fst_name, lst_name, major) VALUES('"+fName+"', '"+lName+"', '"+major+"');";
 
-        try
-        {
-            fh = new FileHandler("src/main/resources/EntryLogFile.log");
-            logger.addHandler(fh);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
+                //Database entry to use in data entry service
+                String entry = "INSERT INTO Registration.Students (fst_name, lst_name, major) VALUES('" + fName + "', '" + lName + "', '" + major + "');";
 
-            logger.info("Connecting to database...");
-            Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement state = con.createStatement();
+                try {
+                    fh = new FileHandler("src/main/resources/EntryLogFile.log");
+                    logger.addHandler(fh);
+                    SimpleFormatter formatter = new SimpleFormatter();
+                    fh.setFormatter(formatter);
 
-            state.execute(entry);
-            logger.info("Data entered...");
+                    logger.info("Connecting to database...");
+                    Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+                    Statement state = con.createStatement();
 
-            state.close();
+                    state.execute(entry);
+                    logger.info("Data entered...");
 
-            logger.info("Done...");
+                    state.close();
 
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }catch (SQLException se) {
-            se.printStackTrace();
-        }
+                    logger.info("Done...");
 
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+            }
+
+        logger.info("Program ended...");
 
     }
 
